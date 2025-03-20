b288241593d2dc59e38a4e9b3b5078ae0a4dc12f
Alex A. Chufistov <146706624+achufistov@users.noreply.github.com>
2025-03-20 16:08:51 +0300

    logic refactoring

--- gtfo-yara.py
+++ gtfo-yara.py
@@ -1,127 +1,121 @@
 import os
 import subprocess
-import logging
-import pwd
-import grp
-import stat
-import yara
-import sys
-import getpass
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from colorama import Fore, Style, init
+import re
 
-red = '\033[31m'
-green = '\033[32m'
-yellow = '\033[0;33m'
-cyan = '\033[0;36m'
-bold = '\033[1m'
-reset = '\033[0m'
+# Инициализация colorama
+init(autoreset=True)
 
-class Logger(logging.Logger):
-    def __init__(self, name, level=logging.DEBUG):
-        super(Logger, self).__init__(name, level)
-        self.console_handler = logging.StreamHandler()
-        self.console_handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(message)s')
-        self.console_handler.setFormatter(formatter)
-        self.addHandler(self.console_handler)
+def find_binaries(directories):
+    binaries = []
+    for directory in directories:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.access(file_path, os.X_OK):  # Проверяем, является ли файл исполняемым
+                    binaries.append(file_path)
+    return binaries
 
-    def set_level(self, level):
-        self.setLevel(level)
-        self.console_handler.setLevel(level)
-
-logging.setLoggerClass(Logger)
-
-log = logging.getLogger(__name__)
-log.set_level(logging.INFO)
-
-
-def get_sudo_password():
-
-    log.info(f"{cyan}{bold}Enter sudo password:\n{reset}")
-    return getpass.getpass("> ")
-
-
-def execute_command(command, sudo_password=None):
-
-    if sudo_password:
-        command = f"echo {sudo_password} | sudo -S {command}"
+def check_gtfobins(binary):
+    binary_name = os.path.basename(binary)
+    url = f"https://gtfobins.github.io/gtfobins/{binary_name}/"
     try:
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        output, error = process.communicate()
-        return output.decode('utf-8'), error.decode('utf-8')
-    except OSError as e:
-        return None, "OS error occurred: " + str(e)
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # Извлекаем только теги <p>, <code>, <h2>
+            content = []
+            for tag in soup.find_all(['p', 'code', 'h2']):
+                if tag.name == 'h2':
+                    content.append(f"\n{tag.text.strip()}\n")
+                elif tag.name == 'p':
+                    # Выводим текст тега <p>, включая содержимое <code>, если оно есть
+                    content.append(f"{tag.text.strip()}\n")
+                elif tag.name == 'code':
+                    # Проверяем, что <code> не находится внутри <p>
+                    if not tag.find_parent('p'):
+                        content.append(f"{tag.text.strip()}\n")
+            return True, "\n".join(content)
+        elif response.status_code == 404:
+            return False, None
+    except Exception as e:
+        print(f"Error while checking GTFOBins for {binary_name}: {e}")
+    return False, None
 
+def run_yara_rule(yara_rule, binaries, rule_name):
+    matched_binaries = set()
+    exploits = []  # Список для хранения потенциальных эксплойтов
+    print(Fore.CYAN + Style.BRIGHT + f"\n=== Starting check with {rule_name} ===")
+    for binary in binaries:
+        try:
+            result = subprocess.run(['yara', yara_rule, binary], capture_output=True, text=True)
+            if result.stdout.strip():  # Если есть совпадения
+                gtfobins_exists, gtfobins_content = check_gtfobins(binary)
+                if gtfobins_exists:
+                    matched_binaries.add(binary)
+                    print(Fore.RED + Style.BRIGHT + f"Check for {binary} failed! Information about how to exploit the privileges for a binary file is available on the GTFOBins resource, visit the resource: https://gtfobins.github.io/gtfobins/{os.path.basename(binary)}/")
+                    print(Fore.YELLOW + Style.BRIGHT + f"Possible exploitation for {binary}:")
+                    print(gtfobins_content)
+                    exploits.append(f"Possible exploitation for {binary}:\n{gtfobins_content}\n")  # Добавляем в список эксплойтов
+                else:
+                    print(Fore.MAGENTA + Style.BRIGHT + f"There is no information about the exploit for {binary} on the GTFOBins resource! Check your yara rule, it may have returned false positive!")
+            else:  # Если совпадений нет
+                print(Fore.GREEN + Style.BRIGHT + f"Check for {binary} passed!")
+        except Exception as e:
+            print(f"Error while checking {binary}: {e}")
+        
+        # Добавляем разделитель после каждого результата
+        print(Fore.CYAN + Style.BRIGHT + "_" * 100)
 
-def check_suid_bins():
+    print(Fore.CYAN + Style.BRIGHT + f"=== Finished check with {rule_name} ===\n")
+    return matched_binaries, exploits  # Возвращаем также список эксплойтов
 
-    suid_bins = []
-    for binary in os.listdir('/usr/bin'):
-        binary_path = os.path.join('/usr/bin', binary)
-        if os.path.isfile(binary_path):
-            file_properties = check_suid_sgid(binary_path)
-            if file_properties.get("SUID") or file_properties.get("SGID"):
-                suid_bins.append({
-                    "Binary": binary,
-                    "Path": binary_path,
-                    "SUID": file_properties.get("Owner") if file_properties.get("SUID") else None,
-                    "SGID": file_properties.get("Group") if file_properties.get("SGID") else None
-                })
-                log.warning(f"Found binary {yellow}{bold}{binary}{reset}: {binary_path}")
-    return suid_bins
-
-
-def check_suid_sgid(file_path):
-
-    try:
-        file_stat = os.stat(file_path)
-        mode = file_stat.st_mode
-        is_suid = bool(mode & stat.S_ISUID)
-        is_sgid = bool(mode & stat.S_ISGID)
-        owner_name = pwd.getpwuid(file_stat.st_uid).pw_name
-        group_name = grp.getgrgid(file_stat.st_gid).gr_name
-        return {"SUID": is_suid, "SGID": is_sgid, "Owner": owner_name, "Group": group_name}
-    except FileNotFoundError:
-        return {"Error": "File not found"}
-
-
-def check_binaries_with_yara(rule_path, binaries, sudo_password):
-
-    rules = yara.compile(filepath=rule_path)
-
-    log.info(f"{cyan}{bold}The verification for each binary has begun...\n{reset}") 
-
-    for binary_info in binaries:
-        binary_path = binary_info['Path']
-        if os.path.isfile(binary_path): 
-            matches = rules.match(binary_path)
-            if matches:
-                log.info(f"{red}{bold}YARA matches for {binary_path}: {matches}") 
-            else:
-                log.info(f"{green}{bold}No matches found for {binary_path}.") 
-        else:
-            log.warning(f"{red}{bold}Skipping {binary_path}, not a file.{reset}")
-
-
-def main():
-    sudo_password = get_sudo_password() 
-    print(f"{cyan}{bold}Searching for SUID/SGID binaries...\n{reset}")
-    suid_binaries = check_suid_bins()
-    if suid_binaries:
-        print(f"{cyan}{bold}\nThe following binary files will be sent for verification by the YARA rule:\n {reset}")
-        for bin_info in suid_binaries:
-            print(f"{yellow}{bold}Binary:{reset} {bin_info['Binary']}, {yellow}{bold}Path:{reset} {bin_info['Path']}, {yellow}{bold}Owner:{reset} {bin_info['SUID']}, {yellow}{bold}Group:{reset} {bin_info['SGID']}")
-
-        print() 
-
-        if len(sys.argv) != 2:
-            print(f"{red}Usage: python script.py <yara_rule.yara>{reset}")
-            sys.exit(1)
-
-        rule_path = sys.argv[1]
-        check_binaries_with_yara(rule_path, suid_binaries, sudo_password)
-    else:
-        print(f"{red}No SUID/SGID binaries found.{reset}")
-
+def clean_output(content):
+    # Удаляем цветовые коды и лишние пробелы
+    return re.sub(r'\x1B\[[0-?9;]*[mK]', '', content).strip()
 
 if __name__ == "__main__":
-    main()
+    # Настройка парсера аргументов
+    parser = argparse.ArgumentParser(description='Check binary files using YARA rules.')
+    parser.add_argument('general_rules', help='Path to the general YARA rule file')
+    parser.add_argument('additional_rules', help='Path to the additional YARA rule file')
+    parser.add_argument('-o', '--output', help='Path to the output file for saving results')
+    args = parser.parse_args()
+
+    # Задаем директории для поиска бинарных файлов
+    directories_to_search = ['/usr/sbin', '/usr/bin']
+
+    # Находим все бинарные файлы
+    binaries = find_binaries(directories_to_search)
+
+    # Начальная надпись
+    print(Fore.CYAN + Style.BRIGHT + "Scanning all binary files in the system and checking against the YARA rules...\n")
+
+    # Запускаем проверку с general_rules.yar
+    general_matches, general_exploits = run_yara_rule(args.general_rules, binaries, "General Rules")
+
+    # Запускаем проверку с debian_additional_rules.yar
+    additional_matches, additional_exploits = run_yara_rule(args.additional_rules, binaries, "Additional Rules")
+
+    # Находим объединение (файлы, которые сработали хотя бы на одно из правил)
+    union_matches = general_matches.union(additional_matches)
+
+    # Сохраняем результаты в файл, если указан аргумент -o
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write("Results for General Rules:\n")
+            f.write("\n".join(general_matches) + "\n\n")
+            f.write("Results for Additional Rules:\n")
+            f.write("\n".join(additional_matches) + "\n\n")
+            f.write("Union (General or Additional Rules):\n")
+            f.write("\n".join(union_matches) + "\n\n")
+
+            # Добавляем секцию с потенциальными эксплойтами
+            f.write("Potential Exploits:\n")
+            f.write("\n".join(general_exploits + additional_exploits) + "\n")
+
+    # Финальное сообщение
+    print(Fore.CYAN + Style.BRIGHT + "Scanning completed. Results saved to the output file." if args.output else "Scanning completed.")
